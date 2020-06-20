40727727b226f2dbe407941fe3331deb31acb085
owencho <owencho200@gmail.com>
2020-06-20 23:07:51 +0800

    fixed the nvic if cases

--- src/Nvic.c
+++ src/Nvic.c
@@ -12,43 +12,43 @@ int arrayNumber;
 int arrayPartLoc;
 void nvicEnableInterrupt(int interruptNum){
     if(interruptNum > 239)
-        return ;
-  	arrayNumber = (interruptNum >> 5) ;
-  	nvic->ISER[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
+        return;
+    arrayNumber = (interruptNum >> 5);
+    nvic->ISER[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
 }
 
 void nvicDisableInterrupt(int interruptNum){
     if(interruptNum > 239)
-        return ;
-  	arrayNumber = (interruptNum >> 5)  ;
-  	nvic->ICER[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
+        return;
+    arrayNumber = (interruptNum >> 5);
+    nvic->ICER[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
 }
 
 void nvicSetPendingInterrupt(int interruptNum){
     if(interruptNum > 239)
-        return ;
-  	arrayNumber = (interruptNum >> 5)  ;
+        return;
+  	arrayNumber = (interruptNum >> 5);
   	nvic->ISPR[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
 }
 
 void nvicClearPendingInterrupt(int interruptNum){
     if(interruptNum > 239)
-        return ;
-  	arrayNumber = (interruptNum >> 5)  ;
+        return;
+  	arrayNumber = (interruptNum >> 5);
   	nvic->ICPR[arrayNumber] |= 1 <<(interruptNum - (arrayNumber << 5));
 }
 
 int nvicIsInterruptActive(int interruptNum){
     if(interruptNum > 239)
         return 0 ; //return error
-  	arrayNumber = (interruptNum >> 5)  ;
+  	arrayNumber = (interruptNum >> 5);
   	return nvic->IABR[arrayNumber] >> (interruptNum - (arrayNumber << 5)) & 0x1;
 }
 
 void nvicSetInterruptPriority(int interruptNum, int interruptPriority){
     if(interruptNum > 239 || interruptNum > 256)
         return ;
-  	arrayNumber = (interruptNum >> 2)  ;
+  	arrayNumber = (interruptNum >> 2);
     arrayPartLoc = 8*(interruptNum - (arrayNumber << 2));
   	nvic->IPR[arrayNumber] |= (interruptPriority <<arrayPartLoc);
 }
